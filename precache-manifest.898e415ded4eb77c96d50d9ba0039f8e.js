019793bf18515696a0d977f290452f83fea7913c
Bartosz Szymański <bartek.szymanski68@gmail.com>
2019-11-08 18:43:02 +0100

    Updates

---
+++ precache-manifest.898e415ded4eb77c96d50d9ba0039f8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c7696cce0333d10277669be3077d7c8",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "1d15aea281c34869d336",
+    "url": "/quiz-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "19c3c5df85564f619aa6",
+    "url": "/quiz-app/static/js/2.98227618.chunk.js"
+  },
+  {
+    "revision": "1d15aea281c34869d336",
+    "url": "/quiz-app/static/js/main.6c4e5111.chunk.js"
+  },
+  {
+    "revision": "d1e240ddabfcc41214f1",
+    "url": "/quiz-app/static/js/runtime-main.803ebfc5.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/quiz-app/static/media/logo.25bf045c.svg"
+  }
+]);
